957ecf95ff1cc5f6c3e8cbf9c5aec36c2cc96e15
Bilgehan Saglik <blghns@users.noreply.github.com>
2026-02-10 20:38:05 +0000

    config change

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/us-inflation-history-chart/',
   plugins: [react(), tailwindcss()],
 })
